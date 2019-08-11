7ed573f941518f1c4abc598b0288b2719c29b23b
j3iiifn <j3iiifn@gmail.com>
2019-08-11 17:24:55 +0900

    Refactor daily_report.py

--- daily_report.py
+++ daily_report.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+import traceback
+import math
+
 INDENT = 4
 MARK_DONE = 'x'
 
@@ -14,27 +18,48 @@ class Node:
         self.flag = False  # print to output_report
         self.todo = True  # print to output_todo
 
+    def __str__(self):
+        return "mark=%s, flag=%s, todo=%s, line=%s" % (self.mark, self.flag, self.todo, self.line)
+
 
 class TaskProcessor:
-    def __init__(self, args):
-        self.in_todo = args.input_todo
-        self.out_report = args.output_report
-        self.out_todo = args.output_todo
+    def __init__(self, output_report=None, output_todo=None, debug=False):
+        self.output_report = output_report
+        self.output_todo = output_todo
+        self.debug = debug
+
+        self.contents = None
+
+        self.root = Node(None, None, None)
+        self.root.flag = False
+        self.root.todo = False
+
+        self.completed_lines = []
+        self.todo_lines = []
 
     def add_flag_to_parent(self, node):
+        if node == self.root:
+            return
+
         node.flag = True
 
         if node.parent:
             self.add_flag_to_parent(node.parent)
 
     def add_flag_to_children(self, node):
+        if node == self.root:
+            return
+
         node.flag = True
         node.todo = False
 
         for c in node.children:
             self.add_flag_to_children(c)
 
-    def check(self, node):
+    def check(self, node=None):
+        if not node:
+            node = self.root
+
         if node.mark == MARK_DONE:
             # if the node is cleared, its children are also cleared regardless of their mark.
             self.add_flag_to_parent(node)
@@ -44,30 +69,49 @@ class TaskProcessor:
         for c in node.children:
             self.check(c)
 
-    def print_node(self, node):
-        if node.flag:
-            print(node.line, file=self.out_report)
+    def create_completed_lines(self, node=None):
+        if not node:
+            node = self.root
 
-        if node.todo:
-            print(node.line, file=self.out_todo)
+        if node == self.root:
+            self.completed_lines = []
+
+        if node.flag:
+            self.completed_lines.append(node.line)
 
         for c in node.children:
-            self.print_node(c)
+            self.create_completed_lines(c)
 
-    def finalize(self, root):
-        self.check(root)
-        self.print_node(root)
+    def create_todo_lines(self, node=None):
+        if not node:
+            node = self.root
+
+        if node == self.root:
+            self.todo_lines = []
+
+        if node.todo:
+            self.todo_lines.append(node.line)
+
+        for c in node.children:
+            self.create_todo_lines(c)
+
+    def finalize(self):
+        self.check(self.root)
+        self.create_completed_lines()
+        print('\n'.join(self.completed_lines), file=self.output_report)
+        self.create_todo_lines()
+        print('\n'.join(self.todo_lines), file=self.output_todo)
+
+    def read_from(self, input_todo):
+        with open(input_todo, 'r') as f:
+            self.contents = f.readlines()
+        self.contents = [x.rstrip() for x in self.contents]
 
     def run(self):
-
-        with open(self.in_todo, 'r') as f:
-            content = f.readlines()
-        content = [x.rstrip() for x in content]
-
-        root = None
-        cursor = None
+        prev_node = None
         prev_level = 0
-        for l in content:
+
+        for l in self.contents:
             try:
                 l_strip = l.lstrip()
                 if not l_strip:
@@ -75,39 +119,37 @@ class TaskProcessor:
                     continue
 
                 leading_spaces = len(l) - len(l_strip)
-                pos_level = leading_spaces / INDENT
+                pos_level = math.floor(leading_spaces / INDENT)
                 mark = l_strip[0]
-                # task = l_strip[1:].lstrip()
-                # print "%d, %s, %s, %s" % (pos_level, mark, task, l)
+                parent = None
     
                 if pos_level == 0:
-                    if root:
-                        self.finalize(root)
-    
-                    node = Node(mark, l, None)
-                    root = node
+                    parent = self.root
     
                 elif prev_level == pos_level:
-                    node = Node(mark, l, cursor.parent)
-                    cursor.parent.children.append(node)
+                    parent = prev_node.parent
     
                 elif prev_level < pos_level:
                     # down 1 level
-                    node = Node(mark, l, cursor)
-                    cursor.children.append(node)
+                    parent = prev_node
     
                 else:
-                    # up 1 level
-                    node = Node(mark, l, cursor.parent.parent)
-                    cursor.parent.parent.children.append(node)
-    
-                cursor = node
-                prev_level = pos_level
-            except:
-                print(l)
-                exit(1)
+                    # up 1 or more levels
+                    parent = prev_node.parent
+                    for i in range(prev_level - pos_level):
+                        parent = parent.parent
 
-        self.finalize(root)
+                pos_node = Node(mark, l, parent)
+                parent.children.append(pos_node)
+
+                prev_node = pos_node
+                prev_level = pos_level
+
+            except Exception as e:
+                print(l, file=sys.stderr)
+                import traceback
+                print(traceback.format_exc(), file=sys.stderr)
+                exit(1)
 
 
 def aparse():
@@ -130,8 +172,10 @@ def aparse():
 
 def main():
     args = aparse()
-    t = TaskProcessor(args)
+    t = TaskProcessor(args.output_report, args.output_todo, args.debug)
+    t.read_from(args.input_todo)
     t.run()
+    t.finalize()
 
 
 if __name__ == "__main__":
